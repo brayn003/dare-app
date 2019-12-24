36258c61c05d9fbc109ff215fda1f50a3e02aa8f
Rohan-B-97 <rohanbhimra+github@gmail.com>
2019-12-24 20:10:10 +0530

    Added Media Uploads

--- components/App.js
+++ components/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
-import {Text} from 'react-native';
-
-const App = () => {
-  return <Text>Helllo Worls</Text>;
-};
-
+import {View} from 'react-native';
+import MediaUpload from './MediaUpload';
+class App extends React.Component {
+  render() {
+    return (
+      // eslint-disable-next-line react-native/no-inline-styles
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <MediaUpload />
+      </View>
+    );
+  }
+}
 export default App;
---
+++ components/MediaUpload.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import {Text, View, StyleSheet, Image, Button} from 'react-native';
+import ImagePicker from 'react-native-image-picker';
+
+class MediaUpload extends React.Component {
+  handleChoosePhoto = () => {
+    const options = {};
+    ImagePicker.launchImaghePicker(options, response => {
+      console.log('response', response);
+    });
+  };
+  render() {
+    return (
+      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <Button title="Choose Photo" onPress={this.handleChoosePhoto} />
+      </View>
+    );
+  }
+}
+// const styles = StyleSheet.create({
+//   title: {
+//     color: 'green',
+//     fontWeight: 'bold',
+//     fontSize: 30,
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//     // flexDirection: 'column',
+//   },
+//   container: {
+//     width: 350,
+//     height: 10,
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     backgroundColor: 'black',
+//   },
+//   image: {
+//     height: 300,
+//     width: 300,
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+//   cardtitle: {
+//     color: 'white',
+//     fontWeight: 'bold',
+//     fontSize: 30,
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+//   description: {
+//     color: 'white',
+//     fontWeight: 'bold',
+//     fontSize: 20,
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+//   footer: {
+//     color: 'green',
+//     fontSize: 15,
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+//   button: {},
+// });
+// export default MediaUpload;
