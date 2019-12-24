705561ee06e6c4a32e12c2055afe83a8a8505008
Reuben <reubenpereira135@gmail.com>
2019-12-24 21:48:38 +0530

    adding sane code

--- components/App.js
+++ components/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
-import {Text} from 'react-native';
-
-const App = () => {
-  return <Text>Helllo Worls</Text>;
-};
+import {View} from 'react-native';
+import Darepage from './Darepage';
+class App extends React.Component {
+  render() {
+    return (
+      // eslint-disable-next-line react-native/no-inline-styles
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <Darepage />
+      </View>
+    );
+  }
+}
 
 export default App;
--- components/Darepage.js
+++ components/Darepage.js
@@ -1,29 +1,30 @@
 import React from 'react';
 import {View, Stylesheet, Text} from 'react-native';
-import {Card, Button} from 'react-native-elements';
+import {Card, TouchableHighlight} from 'react-native';
 
-class Darepage extends React.component {
+class Darepage extends React.Component {
   render() {
     return (
       <View style={Stylesheet.container}>
-        <Button>
+        <TouchableHighlight>
+          <Card>
+            title='DARE.INC' image={require('../images/pic2.jpg')}>
+            <Text style={{marginBottom: 10}}>
+              The idea with React Native Elements is more about
+            </Text>
+          </Card>
+        </TouchableHighlight>
+        <TouchableHighlight>
           <Card>
             title='HELLO WORLD' image={require('../images/pic2.jpg')}>
             <Text style={{marginBottom: 10}}>
               The idea with React Native Elements is more about
             </Text>
           </Card>
-        </Button>
-        <Button>
-          <Card>
-            title='HELLO WORLD' image={require('../images/pic2.jpg')}>
-            <Text style={{marginBottom: 10}}>
-              The idea with React Native Elements is more about
-            </Text>
-          </Card>
-        </Button>
+        </TouchableHighlight>
       </View>
     );
   }
 }
+
 export default Darepage;
