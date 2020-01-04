7e7c1872564336f47f35c18ed4a436f57ffb9658
Rohan-B-97 <rohanbhimra+github@gmail.com>
2020-01-04 19:23:29 +0530

    adding media upload

--- components/App.js
+++ components/App.js
@@ -1,30 +1,15 @@
 import React from 'react';
-<<<<<<< HEAD
+// import {NativeRouter} from 'react-router-native';
 import {View} from 'react-native';
-import Accepted from './Accepted';
-import Success from './Success';
 import MediaUpload from './MediaUpload';
-class App extends React.Component {
-  render() {
-    return (
-      // eslint-disable-next-line react-native/no-inline-styles
-      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-        <Accepted />
-        <Success />
-        <MediaUpload />
-      </View>
-=======
-import {NativeRouter} from 'react-router-native';
-
 import CoreLayout from './CoreLayout';
 
 class App extends React.Component {
   render() {
     return (
-      <NativeRouter>
-        <CoreLayout />
-      </NativeRouter>
->>>>>>> 90d8f1853caacd839b09749cda52b485aa6bdb0d
+      <View>
+        <MediaUpload />
+      </View>
     );
   }
 }
--- components/MediaUpload.js
+++ components/MediaUpload.js
@@ -1,66 +1,65 @@
 import React from 'react';
-import {Text, View, StyleSheet, Image, Button} from 'react-native';
+import {View, Image, Button} from 'react-native';
 import ImagePicker from 'react-native-image-picker';
 
-class MediaUpload extends React.Component {
+export default class App extends React.Component {
+  state = {
+    photo: null,
+  };
+
   handleChoosePhoto = () => {
-    const options = {};
-    ImagePicker.launchImaghePicker(options, response => {
-      console.log('response', response);
+    const options = {
+      noData: true,
+    };
+    ImagePicker.launchImageLibrary(options, response => {
+      if (response.uri) {
+        this.setState({photo: response});
+      }
     });
   };
+
   render() {
+    const {photo} = this.state;
     return (
-      <View
-        style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        {photo && (
+          <Image source={{uri: photo.uri}} style={{width: 300, height: 300}} />
+        )}
         <Button title="Choose Photo" onPress={this.handleChoosePhoto} />
       </View>
     );
   }
 }
-// const styles = StyleSheet.create({
-//   title: {
-//     color: 'green',
-//     fontWeight: 'bold',
-//     fontSize: 30,
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//     // flexDirection: 'column',
-//   },
-//   container: {
-//     width: 350,
-//     height: 10,
-//     flex: 1,
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//     backgroundColor: 'black',
-//   },
-//   image: {
-//     height: 300,
-//     width: 300,
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-//   cardtitle: {
-//     color: 'white',
-//     fontWeight: 'bold',
-//     fontSize: 30,
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-//   description: {
-//     color: 'white',
-//     fontWeight: 'bold',
-//     fontSize: 20,
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-//   footer: {
-//     color: 'green',
-//     fontSize: 15,
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-//   button: {},
-// });
-// export default MediaUpload;
+
+// import React from 'react';
+// import {Text, View, StyleSheet, Image, Button} from 'react-native';
+// import ImagePicker from 'react-native-image-picker';
+// import OutlineButton from './commons/Button';
+
+// class MediaUpload extends React.Component {
+//   state = {
+//     photo: null,
+//   };
+
+//   handleChoosePhoto = () => {
+//     const options = {
+//       noData: true,
+//     };
+//     ImagePicker.launchImageLibrary(options, response => {
+//       if (response.uri) {
+//         this.setState({photo: response});
+//       }
+//     });
+//   };
+//   render() {
+//     const { photo } = this.state;
+//     return (
+//       <View>
+//         {photo && (
+//           <Image source={{uri: photo.uri}} style={{width: 300, height: 300}} />
+//         )}
+//         <Button title="Choose Photo" onPress={this.handleChoosePhoto} />
+//       </View>
+//     );
+//   }
+// }
